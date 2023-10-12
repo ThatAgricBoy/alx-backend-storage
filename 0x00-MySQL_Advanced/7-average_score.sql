1cb8e605bb6f598e546fd6e3a662bf6206a2f078
ThatAgricBoy <samueljohn3999@gmail.com>
2023-10-12 14:58:52 +0100

    7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,31 +1,13 @@
--- SQL script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student. Note: An average score can be a decimal
-DELIMITER //
-
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+-- Task 7: Average score - creates a stored procedure ComputeOverallScoreForUser
+-- that computes and store the overall score for a student.
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT)
 BEGIN
-    DECLARE total_score DECIMAL(10, 2);
-    DECLARE total_projects INT;
-    DECLARE average DECIMAL(10, 2);
-
-    -- Compute total score for the user
-    SELECT SUM(score) INTO total_score
-    FROM corrections
-    WHERE user_id = user_id;
-
-    -- Compute total projects for the user
-    SELECT COUNT(*) INTO total_projects
-    FROM corrections
-    WHERE user_id = user_id;
-
-    -- Compute average score
-    IF total_projects > 0 THEN
-        SET average = total_score / total_projects;
-        UPDATE users
-        SET average_score = average
-        WHERE id = user_id;
-    ELSE
-        SET average = 0;
-    END IF;
-END //
-
+    DECLARE avg_score FLOAT;
+    SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
+    UPDATE users SET average_score = avg_score WHERE id=user_id;
+END
+$$
 DELIMITER ;
